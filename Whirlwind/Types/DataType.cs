fa0ae5e8a435e3ac7e42a30507905c5186fa6c37
ComedicChimera <forlornsisu@gmail.com>
2018-07-06 02:09:42 -0500

    renamed lock to volatile

---
+++ Whirlwind/Types/DataType.cs
@@ -0,0 +1,19 @@
+﻿namespace Whirlwind.Types
+{
+    interface IDataType
+    {
+        bool Coerce(IDataType other);
+        string Classify();
+    }
+
+    static class TypeCast
+    {
+        static bool DynamicCast(IDataType dt1, IDataType dt2) => false;
+
+        // add literal parameter once valid
+        static bool StaticCast() => false;
+
+        static bool ValidityCast(IDataType dt1, IDataType dt2) => false;
+    }
+
+}
---
+++ Whirlwind/Types/SimpleType.cs
@@ -0,0 +1,52 @@
+﻿using System.Linq;
+
+namespace Whirlwind.Types
+{
+    class SimpleType : IDataType
+    {
+        public enum DataType
+        {
+            INTEGER,
+            FLOAT,
+            BOOL,
+            STRING,
+            CHAR,
+            BYTE,
+            LONG,
+            TYPE
+        }
+
+        public DataType Type { get; private set; }
+
+        public SimpleType(DataType dt)
+        {
+            Type = dt;
+        }
+
+        public string Classify() => "SIMPLE_TYPE";
+
+        public bool Coerce(IDataType other)
+        {
+            if (other.Classify() == "SIMPLE_TYPE")
+            {
+                if (((SimpleType)other).Type == Type)
+                    return true;
+                switch (((SimpleType)other).Type)
+                {
+                    // integer to long and float
+                    case DataType.INTEGER:
+                        return new[] { DataType.FLOAT, DataType.LONG }.Contains(Type);
+                    // char to integer and string
+                    case DataType.CHAR:
+                        return new[] { DataType.STRING, DataType.INTEGER }.Contains(Type);
+                    // byte to everything except boolean and data type
+                    case DataType.BYTE:
+                        return !new[] { DataType.BOOL, DataType.TYPE }.Contains(Type);
+                }
+            }
+            else if (other.Classify() == "UNION")
+                return other.Coerce(this);
+            return false; 
+        }
+    }
+}
