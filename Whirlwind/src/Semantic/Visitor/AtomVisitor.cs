da1d176d139122612ab0cdcc2d17e7b2775a98b0
ComedicChimera <forlornsisu@gmail.com>
2018-08-04 14:10:27 -0500

    finished all trailer checking

--- Whirlwind/src/Semantic/Visitor/AtomVisitor.cs
+++ Whirlwind/src/Semantic/Visitor/AtomVisitor.cs
@@ -132,9 +132,70 @@ namespace Whirlwind.Semantic.Visitor
                         else
                             throw new SemanticException("The '->' operator is not valid the given type", node.Content[0].Position);
                     case "[":
+                        _visitSubscript(root.Type, node);
                         break;
                     case "{":
-                        break;
+                        int initCount = 1;
+                        var positions = new List<TextPosition>();
+                        foreach (var item in ((ASTNode)node.Content[0]).Content)
+                        {
+                            if (item.Name == "TOKEN")
+                            {
+                                if (((TokenNode)item).Tok.Type == "IDENTIFIER")
+                                {
+                                    positions.Add(item.Position);
+                                    _nodes.Add(new ValueNode("Identifier", new SimpleType(), ((TokenNode)item).Tok.Value));
+                                }
+                                    
+                            }
+                            else if (item.Name == "initializer")
+                            {
+                                _visitExpr((ASTNode)((ASTNode)item).Content[1]);
+                                _nodes.Add(new TreeNode("Initializer", _nodes.Last().Type));
+                                PushForward(2);
+                                initCount++;
+                            }
+                        }
+                        if (root.Type.Classify() == "MODULE")
+                        {
+                            var moduleInstance = ((ModuleType)root.Type).GetInstance();
+                            for (int i = 1; i < initCount; i++)
+                            {
+                                var item = (TreeNode)_nodes[_nodes.Count - i];
+                                if (moduleInstance.GetProperty(((ValueNode)item.Nodes[0]).Value, out Symbol sym))
+                                {
+                                    if (!sym.DataType.Coerce(item.Type))
+                                        throw new SemanticException($"Unable to initializer property {sym.Name} with the given type", positions[i - 1]);
+                                }
+                                else
+                                    throw new SemanticException($"Module has no public property {((ValueNode)item.Nodes[0]).Value}", positions[i - 1]);
+                            }
+                            _nodes.Add(new TreeNode("InitList", moduleInstance));
+                            // add in root
+                            PushForward(initCount + 1);
+                            break;
+                        }
+                        else if (root.Type.Classify() == "STRUCT")
+                        {
+                            var members = ((StructType)root).Members;
+                            if (initCount != members.Count)
+                                throw new SemanticException("Struct initializer list must initialize all struct members", node.Content[0].Position);
+                            for (int i = 1; i < initCount; i++)
+                            {
+                                var item = (TreeNode)_nodes[_nodes.Count - i];
+                                string name = ((ValueNode)item.Nodes[0]).Value;
+                                if (!members.ContainsKey(name))
+                                    throw new SemanticException($"Struct has no member {name}", positions[i - 1]);
+                                if (!members[name].Coerce(item.Nodes[1].Type))
+                                    throw new SemanticException($"Unable to initialize member {name} with the given type", positions[i - 1]);
+                            }
+                            ((StructType)root.Type).Instantiate();
+                            _nodes.Add(new TreeNode("InitList", root.Type));
+                            // add in root
+                            PushForward(initCount + 1);
+                            break;
+                        }
+                        throw new SemanticException("Unable to apply initializer list to the given type", node.Position);
                     case "(":
                         _visitFunctionCall(node, root);
                         break;
@@ -256,6 +317,167 @@ namespace Whirlwind.Semantic.Visitor
                 // add function to beginning of call
                 ((TreeNode)_nodes[_nodes.Count - 1]).Nodes.Insert(0, _nodes[_nodes.Count - 2]);
                 _nodes.RemoveAt(_nodes.Count - 2);
+            }
+        }
+
+        private void _visitSubscript(IDataType rootType, ASTNode node)
+        {
+            bool hasStartingExpr = false;
+            int expressionCount = 0, colonCount = 0;
+            var types = new List<IDataType>();
+            var textPositions = new List<TextPosition>();
+
+            foreach (var item in node.Content)
+            {
+                if (item.Name == "expr")
+                {
+                    _visitExpr((ASTNode)item);
+                    hasStartingExpr = true;
+                    expressionCount++;
+                    types.Add(_nodes.Last().Type);
+                    textPositions.Add(item.Position);
+                }
+                else if (item.Name == "slice")
+                {
+                    foreach (var subNode in ((ASTNode)item).Content)
+                    {
+                        if (subNode.Name == "expr")
+                        {
+                            _visitExpr((ASTNode)subNode);
+                            expressionCount++;
+                            types.Add(_nodes.Last().Type);
+                            textPositions.Add(item.Position);
+                        }
+                        // only token is colon
+                        else if (subNode.Name == "TOKEN")
+                            colonCount++;
+                    }
+                }
+            }
+
+            string name;
+            if (hasStartingExpr)
+            {
+                switch (expressionCount)
+                {
+                    case 1:
+                        if (colonCount == 0)
+                            name = "Subscript";
+                        else
+                            name = "SliceBegin";
+                        break;
+                    case 2:
+                        if (colonCount == 1)
+                            name = "Slice";
+                        else
+                            name = "SliceBeginStep";
+                        break;
+                    // 3
+                    default:
+                        name = "SliceStep";
+                        break;
+                }
+            }
+            else
+            {
+                switch (expressionCount)
+                {
+                    case 1:
+                        name = "SliceEnd";
+                        break;
+                    // 2
+                    default:
+                        name = "SliceEndStep";
+                        break;
+                }
+            }
+
+            if (rootType.Classify() == "MAP")
+            {
+                if (name != "Subscript")
+                    throw new SemanticException("Unable to perform slice on a map", node.Position);
+                if (((MapType)rootType).KeyType.Coerce(_nodes.Last().Type))
+                {
+                    _nodes.Add(new TreeNode(name, ((MapType)rootType).ValueType));
+                    PushForward(2);
+                }
+                else
+                    throw new SemanticException("The subscript type and the key type must match", textPositions[0]);
+            }
+            else if (rootType.Classify() == "MODULE_INSTANCE")
+            {
+                if (((ModuleInstance)rootType).GetProperty(name == "Subscript" ? "__subscript__" : "__slice__", out Symbol symbol))
+                {
+                    if (symbol.DataType.Classify() == "FUNCTION")
+                    {
+                        var args = new List<ParameterValue>();
+
+                        switch (name)
+                        {
+                            case "Subscript":
+                            case "SliceBegin":
+                                args.Add(new ParameterValue(types[0]));
+                                break;
+                            case "SliceEnd":
+                                args.Add(new ParameterValue("end", types[0]));
+                                break;
+                            case "SliceEndStep":
+                                args.Add(new ParameterValue("end", types[0]));
+                                args.Add(new ParameterValue("step", types[1]));
+                                break;
+                            case "SliceBeginStep":
+                                args.Add(new ParameterValue(types[0]));
+                                args.Add(new ParameterValue("step", types[1]));
+                                break;
+                            case "SliceStep":
+                                args.Add(new ParameterValue("step", types[0]));
+                                break;
+                            case "Slice":
+                                args.AddRange(types.Select(x => new ParameterValue(x)));
+                                break;
+                        }
+
+                        if (CheckParameters((FunctionType)symbol.DataType, args))
+                        {
+                            _nodes.Add(new TreeNode("OverloadCall", ((FunctionType)symbol.DataType).ReturnType, new List<ITypeNode>()));
+                            // capture root as well
+                            PushForward(args.Count + 1);
+                            // insert in call name
+                            ((TreeNode)_nodes[_nodes.Count - 1]).Nodes.Insert(1, new ValueNode("Identifier", new SimpleType(), name == "Subscript" ? "__subscript__" : "__slice__"));
+                        }
+                        else
+                            throw new SemanticException("The given module defines an invalid overload for the `[]` operator", node.Position);
+                    }
+                    else
+                        throw new SemanticException("The given module defines no overload for the `[]` operator", node.Position);
+                }
+            }
+            else
+            {
+                var intType = new SimpleType(SimpleType.DataType.INTEGER);
+                if (!types.All(x => intType.Coerce(x))) {
+                    throw new SemanticException($"Invalid index type for {(expressionCount == 1 && hasStartingExpr ? "subscript" : "slice")} ", 
+                        textPositions[Enumerable.Range(0, expressionCount).Where(x => !intType.Coerce(types[x])).First()]
+                        );
+                }
+
+                IDataType elementType;
+                switch (rootType.Classify())
+                {
+                    case "ARRAY_TYPE":
+                        elementType = ((ArrayType)rootType).ElementType;
+                        break;
+                    case "LIST_TYPE":
+                        elementType = ((ListType)rootType).ElementType;
+                        break;
+                    default:
+                        throw new SemanticException($"Unable to perform  {(expressionCount == 1 && hasStartingExpr ? "subscript" : "slice")} on the given type", 
+                            node.Position);
+                }
+
+                _nodes.Add(new TreeNode(name, name == "Subscript" ? elementType : rootType));
+                // capture root as well
+                PushForward(expressionCount + 1);
             }
         }
     }
