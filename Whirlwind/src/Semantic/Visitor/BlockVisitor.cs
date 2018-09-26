8eee78908ad4eb3a3c819cbb4f6d84c9d011cb60
ComedicChimera <forlornsisu@gmail.com>
2018-09-25 23:13:49 -0500

    started work on block visitor

--- Whirlwind/src/Semantic/Visitor/BlockVisitor.cs
+++ Whirlwind/src/Semantic/Visitor/BlockVisitor.cs
@@ -7,7 +7,34 @@ namespace Whirlwind.Semantic.Visitor
 {
     partial class Visitor
     {
-        private void _visitBlock(ASTNode node)
+        private void _visitBlock(ASTNode node, StatementContext context)
+        {
+            ASTNode stmt = (ASTNode)node.Content[0];
+
+            switch (stmt.Name)
+            {
+                case "stmt":
+                    _visitStatement(stmt, context);
+                    break;
+                case "block_stmt":
+                    ASTNode blockStatement = (ASTNode)stmt.Content[0];
+
+                    switch (blockStatement.Name)
+                    {
+                        case "if_stmt":
+                            _visitIf(blockStatement);
+                            break;
+                    }
+
+                    break;
+                case "func_stmt":
+                    break;
+                case "subscope":
+                    break;
+            }
+        }
+
+        private void _visitIf(ASTNode ifStmt)
         {
 
         }
