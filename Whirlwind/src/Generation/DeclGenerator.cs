6fe3b2cca7ee2a4ba4f32a4e2207e17952cb3780
ComedicChimera <forlornsisu@gmail.com>
2019-10-21 22:42:19 -0500

    worked on generator

--- Whirlwind/src/Generation/DeclGenerator.cs
+++ Whirlwind/src/Generation/DeclGenerator.cs
@@ -21,33 +21,65 @@ namespace Whirlwind.Generation
             var llvmStruct = LLVM.StructCreateNamed(_ctx, name);
             llvmStruct.StructSetBody(st.Members.Select(x => _convertType(x.Value.DataType)).ToArray(), packed);
 
+            var memberDict = new Dictionary<string, ITypeNode>();
+            bool needsInitMembers = false;
+
             // process constructor
-            bool hasMultipleConstructors = node.Block.Count > 1;
+            bool hasMultipleConstructors = node.Block.Where(x => x is BlockNode).Count() > 1;
             foreach (var item in node.Block)
             {
-                if (item is ExprNode)
-                    continue;
-
-                BlockNode constructor = (BlockNode)item;
-                FunctionType cft = (FunctionType)(constructor.Nodes[0].Type);
-                cft.ReturnType = st;
-                string suffix = ".constructor";
-
-                if (hasMultipleConstructors)
-                    suffix += "." + string.Join(",", cft.Parameters.Select(x => x.DataType.LLVMName()));
-
-                var llvmConstructor = _generateFunctionPrototype(name + suffix, cft, exported);
-
-                if (constructor.Block.Count > 0)
+                if (item is ExprNode eNode)
                 {
-                    LLVM.PositionBuilderAtEnd(_builder, LLVM.AppendBasicBlockInContext(_ctx, llvmConstructor, "entry"));
+                    if (memberDict.Count == 0)
+                        needsInitMembers = true;
 
-                    _generateBlock(node.Block);
-
-                    // build return type here
-
-                    LLVM.VerifyFunction(llvmConstructor, LLVMVerifierFailureAction.LLVMPrintMessageAction);
+                    foreach (var elem in eNode.Nodes.Skip(1))
+                        memberDict.Add(((IdentifierNode)elem).IdName, eNode.Nodes[0]);
                 }
+                else
+                {
+                    BlockNode constructor = (BlockNode)item;
+                    FunctionType cft = (FunctionType)(constructor.Nodes[0].Type);
+                    cft.ReturnType = st;
+                    string suffix = ".constructor";
+
+                    if (hasMultipleConstructors)
+                        suffix += "." + string.Join(",", cft.Parameters.Select(x => x.DataType.LLVMName()));
+
+                    var llvmConstructor = _generateFunctionPrototype(name + suffix, cft, exported);
+
+                    if (constructor.Block.Count > 0)
+                    {
+                        LLVM.PositionBuilderAtEnd(_builder, LLVM.AppendBasicBlockInContext(_ctx, llvmConstructor, "entry"));
+
+                        // build new struct here
+
+                        // build init members call
+                        if (needsInitMembers)
+                        {
+
+                        }
+
+                        _generateBlock(node.Block);
+
+                        LLVM.VerifyFunction(llvmConstructor, LLVMVerifierFailureAction.LLVMPrintMessageAction);
+                    }
+                }
+            }
+
+            if (needsInitMembers)
+            {
+                var initFn = _generateFunctionPrototype(name + ".$_initMembers", new FunctionType(new List<Parameter>
+                            { new Parameter("$THIS", new PointerType(st, false), false, false, false, false) },
+                           new NoneType(), false), false);
+
+                LLVM.PositionBuilderAtEnd(_builder, LLVM.AppendBasicBlockInContext(_ctx, initFn, "entry"));
+
+                // build init members content
+
+                LLVM.BuildRetVoid(_builder);
+
+                LLVM.VerifyFunction(initFn, LLVMVerifierFailureAction.LLVMPrintMessageAction);
             }
         }
 
