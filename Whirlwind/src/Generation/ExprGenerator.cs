cc2c842ca634c8771ec019aac9c5405ae0919e4b
ComedicChimera <forlornsisu@gmail.com>
2019-10-22 21:52:12 -0500

    worked on generator

--- Whirlwind/src/Generation/ExprGenerator.cs
+++ Whirlwind/src/Generation/ExprGenerator.cs
@@ -13,7 +13,40 @@ namespace Whirlwind.Generation
     {
         private LLVMValueRef _generateExpr(ITypeNode expr)
         {
-            // for now
+            if (expr is ValueNode vn)
+                return _generateExprValue(vn);
+
+            return LLVM.ConstInt(LLVM.Int32Type(), 0, new LLVMBool(0));
+        }
+
+        private LLVMValueRef _generateExprValue(ValueNode node)
+        {
+            switch (node.Name)
+            {
+                case "Literal":
+                    {
+                        var st = (SimpleType)node.Type;
+
+                        switch (st.Type)
+                        {
+                            case SimpleType.SimpleClassifier.BOOL:
+                                return LLVM.ConstInt(_convertType(node.Type), (ulong)(node.Value == "true" ? 1 : 0), new LLVMBool(0));
+                            case SimpleType.SimpleClassifier.CHAR:
+                            case SimpleType.SimpleClassifier.INTEGER:
+                            case SimpleType.SimpleClassifier.LONG:
+                                return LLVM.ConstInt(_convertType(node.Type), ulong.Parse(node.Value), new LLVMBool(Convert.ToInt32(st.Unsigned)));
+                            case SimpleType.SimpleClassifier.FLOAT:
+                            case SimpleType.SimpleClassifier.DOUBLE:
+                                return LLVM.ConstRealOfString(_convertType(node.Type), node.Value);
+                            case SimpleType.SimpleClassifier.STRING:
+                                // for now
+                                return LLVM.ConstInt(LLVM.Int32Type(), 0, new LLVMBool(0));
+                        }
+                    }
+                    break;
+            }
+
+            // other values a bit more complicated
             return LLVM.ConstInt(LLVM.Int32Type(), 0, new LLVMBool(0));
         }
     }
