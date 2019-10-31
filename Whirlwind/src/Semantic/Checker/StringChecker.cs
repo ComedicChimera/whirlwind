740e751d84a40726434f923c568317c8b70a218c
ComedicChimera <forlornsisu@gmail.com>
2019-10-31 09:36:00 -0500

    working unicode string and char verification

--- Whirlwind/src/Semantic/Checker/StringChecker.cs
+++ Whirlwind/src/Semantic/Checker/StringChecker.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
 
 namespace Whirlwind.Semantic.Checker
@@ -21,13 +21,52 @@ namespace Whirlwind.Semantic.Checker
                 else
                     return false;
             }
-            // handle unicode strings
             else
-                return charData.Length == 1;
+            {
+                byte[] bytes = Encoding.UTF8.GetBytes(charData);
+
+                if (bytes.Length == 1)
+                    return true;
+                else if (bytes.Length == 2)
+                    return bytes[0] >= 0xC2;
+                
+                // handle unicode in accordance with table
+                // (https://lemire.me/blog/2018/05/09/how-quickly-can-you-check-that-a-string-is-valid-unicode-utf-8/)
+                else
+                    return false;
+            }
         }
 
         public static bool VerifyString(string stringLit)
         {
+            int expectHex = 0;
+            bool expectEscapeChar = false;
+
+            string hexChars = "0123456789ABCDEF";
+            string escapeChars = "ab0fntv\"\'\\0s";
+
+            foreach (char ch in stringLit.Substring(1, stringLit.Length - 1))
+            {
+                if (expectEscapeChar)
+                {
+                    if (ch == 'u')
+                        expectHex = 4;
+                    else if (ch == 'U')
+                        expectHex = 8;
+                    else if (!escapeChars.Contains(ch))
+                        return false;
+                }
+                else if (expectHex > 0)
+                {
+                    if (!hexChars.Contains(ch))
+                        return false;
+
+                    expectHex--;
+                }
+                else if (ch == '\\')
+                    expectEscapeChar = true;
+            }
+
             return true;
         }
     }
