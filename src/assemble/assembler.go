6c0528ba8fea65e7443736f3290c1dededc8203a
ComedicChimera <forlornsisu@gmail.com>
2020-09-30 10:57:36 -0500

    small update to pkg assembler

--- src/assemble/assembler.go
+++ src/assemble/assembler.go
@@ -1,9 +1,9 @@
 package assemble
 
-// Assembler is the construct responsible for managing package assembly --
+// PAssembler is the construct responsible for managing package assembly --
 // construction of top-level of a package or packages.  It is the implementation
 // of compilation Stage 2.
-type Assembler struct {
+type PAssembler struct {
 	// Packages is the map of packages that are being assembled (by ID) along
 	// with their associated resolution table.  The package ref is embedded in
 	// the resolution table so we don't need to store it two times.
@@ -11,4 +11,22 @@ type Assembler struct {
 
 	// DefQueue is the queue of definitions being resolved in all packages
 	DefQueue *DefinitionQueue
+}
+
+// NewPackageAssembler returns a new PAssembler for the given packages
+func NewPackageAssembler(pkgs []*common.WhirlPackage) (*PAssembler) {
+	pa := &PAssembler{Packages: make(map[uint]*ResolutionTable)}
+	for _, pkg := range pkgs {
+		pa.Packages[pkg.PackageID] = &ResolutionTable{CurrPkg: pkg}
+	}
+	pa.DefQueue = &DefinitionQueue{}
+	return pa
+}
+
+// Assemble runs the main package assembly algorithm
+func (pa *PAssember) Assemble() {
+	for pkgid, rtable := range pa.Packages {
+		// TODO
+		_, _ := pkgid, rtable
+	}
 }
