841fca2b3011d34385e783e106feef5291732ad3
ComedicChimera <forlornsisu@gmail.com>
2020-12-12 23:17:31 -0600

    continued work on type system

--- src/typing/types.go
+++ src/typing/types.go
@@ -7,11 +7,10 @@ package typing
 // 3. Vectors -- A n-length, uniform type array of numeric primitives
 // 4. References -- A type referencing a value through a pointer
 // 5. Structures -- A record of named, typed fields
-// 6. Type Sets -- A type which can assume the form of any type in its set
-// 7. Algebraic Types - A type that contains a finite number of enumerated
-// values (that can contain sub-values).
-// All of these types are members of the type interface; interfaces are
-// considered type sets, builtin collections are considered structures.
+// 6. Interfaces -- A type that groups types based on shared behavior
+// 7. Algebraic Types - A type that contains a finite number of enumerated values
+// 8. Type Sets -- A set/union of multiple type values
+// 9. Regions -- The typing of a region literal
 
 // DataType is the general interface for all data types
 type DataType interface {
@@ -49,14 +48,37 @@ const (
 	PrimIntLong
 )
 
+// TupleType represents a tuple
+type TupleType []DataType
+
 // VectorType represents a vector
 type VectorType struct {
 	ElemType DataType
 	Size     uint
 }
 
-// TupleType represents a tuple
-type TupleType []DataType
+// RefType represents a reference type
+type RefType struct {
+	ElemType        DataType
+	Block, Constant bool
+	Owned           bool
+
+	// This field may remain nil until memory analysis occurs which means that
+	// it is not a good indicator of whether or not a reference is free thus the
+	// additional `Owned` field.
+	Region *RegionType
+}
+
+// RegionType represents the typing of a region literal
+type RegionType struct {
+	Id, Rank int
+
+	// This field can be nil if there is no parent
+	Parent *RegionType
+
+	// This slice may be empty if there are no children
+	Children []*RegionType
+}
 
 // FuncType represents a function
 type FuncType struct {
@@ -68,9 +90,10 @@ type FuncType struct {
 
 // StructType represents a structure type
 type StructType struct {
-	Name    string
-	Members map[string]*TypeValue
-	Packed  bool
+	Name     string
+	Fields   map[string]*TypeValue
+	Packed   bool
+	Inherits []*StructType
 }
 
 // TypeValue represents a value-like component of a type
@@ -78,3 +101,48 @@ type TypeValue struct {
 	Type               DataType
 	Constant, Volatile bool
 }
+
+// InterfType represents an interface type
+type InterfType struct {
+	Methods map[string]*InterfMethod
+
+	// Indicates whether the interface is a type interface or not
+	TypeInterf bool
+
+	// Implements can also be generic thus DataType instead of *InterfType
+	Implements []DataType
+
+	// Instances lists the various interfaces that implement/are instances of this interface
+	Instances []DataType
+}
+
+// InterfMethod represents a method in an interface
+type InterfMethod struct {
+	// Methods can be generic so we accept any type here
+	Signature DataType
+
+	// Can be any one of the enumerated method kinds below
+	Kind int
+}
+
+const (
+	MKVirtual   = iota // Method that is given a body in a parent interface
+	MKOveride          // Method that overrides a virtual method implementation in a derived interf
+	MKAbstract         // Method that is defined without a body in a parent interface (to be defined)
+	MKImplement        // Method that implements an abstract method
+	MKStandard         // Method that is defined on a type interface that is not an override or abstract implementation
+)
+
+// AlgebraicType represents an algebraic type
+type AlgebraicType struct {
+	Instances map[string]*AlgebraicInstance
+}
+
+// AlgebraicInstance is a type that is an instance of a larger algebraic type
+type AlgebraicInstance struct {
+	Name   string
+	Values []DataType
+}
+
+// TypeSet represents a type set
+type TypeSet []DataType
