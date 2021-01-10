b09456a55164d970b5b29aa0777a647f8f50e570
ComedicChimera <forlornsisu@gmail.com>
2021-01-10 01:13:11 -0600

    worked on type label walking

--- src/validate/type_walker.go
+++ src/validate/type_walker.go
@@ -1,6 +1,10 @@
 package validate
 
 import (
+	"fmt"
+
+	"github.com/ComedicChimera/whirlwind/src/common"
+	"github.com/ComedicChimera/whirlwind/src/logging"
 	"github.com/ComedicChimera/whirlwind/src/syntax"
 	"github.com/ComedicChimera/whirlwind/src/typing"
 )
@@ -51,5 +55,105 @@ func (w *Walker) walkTypeExt(ext *syntax.ASTBranch) (typing.DataType, bool) {
 // walkTypeLabel walks and attempts to extract a data type from a type label. If
 // this function fails, it will set `fatalDefError` appropriately.
 func (w *Walker) walkTypeLabel(label *syntax.ASTBranch) (typing.DataType, bool) {
+	typeCat := label.BranchAt(0)
+	switch typeCat.Name {
+	case "named_type":
+		var rootName, accessedName string
+		var rootPos, accessedPos *logging.TextPosition
+
+		for _, item := range typeCat.Content {
+			switch v := item.(type) {
+			case *syntax.ASTLeaf:
+				if v.Kind == syntax.IDENTIFIER {
+					if rootName == "" {
+						rootName = v.Value
+						rootPos = v.Position()
+					} else {
+						accessedName = v.Value
+						accessedPos = v.Position()
+					}
+				}
+			case *syntax.ASTBranch:
+				if v.Name == "type_list" {
+
+				}
+			}
+		}
+
+		return w.walkNamedTypeCore(rootName, accessedName, rootPos, accessedPos)
+	}
+
+	return nil, false
+}
+
+// walkNamedTypeCore walks and accesses the named data type at the core of the `named_type` node
+func (w *Walker) walkNamedTypeCore(rootName, accessedName string, rootPos, accessedPos *logging.TextPosition) (typing.DataType, bool) {
+	if accessedName == "" {
+		if symbol, ok := w.Lookup(rootName); ok {
+			if symbol.DefKind != common.DefKindTypeDef {
+				w.logFatalDefError(
+					fmt.Sprintf("Symbol `%s` is not a type", symbol.Name),
+					logging.LMKUsage,
+					rootPos,
+				)
+
+				return nil, false
+			}
+
+			if w.DeclStatus == common.DSExported {
+				if symbol.VisibleExternally() {
+					return symbol.Type, true
+				}
+
+				w.logFatalDefError(
+					fmt.Sprintf("Symbol `%s` must be exported to be used in an exported definition", symbol.Name),
+					logging.LMKUsage,
+					rootPos,
+				)
+
+				return nil, false
+			}
+
+			return symbol.Type, true
+		} else {
+			w.LogUndefined(rootName, rootPos)
+			w.fatalDefError = true
+
+			return nil, false
+		}
+	} else if w.DeclStatus == common.DSExported {
+		w.logFatalDefError(
+			"Unable to use implicitly imported symbol in exported definition",
+			logging.LMKUsage,
+			accessedPos,
+		)
+	}
+
+	if pkg, ok := w.SrcFile.VisiblePackages[rootName]; ok {
+		if symbol, ok := pkg.ImportFromNamespace(accessedName); ok {
+			if symbol.DefKind != common.DefKindTypeDef {
+				w.logFatalDefError(
+					fmt.Sprintf("Symbol `%s` is not a type", symbol.Name),
+					logging.LMKUsage,
+					accessedPos,
+				)
+
+				return nil, false
+			}
+
+			return symbol.Type, true
+		} else {
+			w.LogNotVisibleInPackage(accessedName, rootName, accessedPos)
+			w.fatalDefError = true
+
+			return nil, false
+		}
+	}
+
+	w.logFatalDefError(
+		fmt.Sprintf("Package `%s` is not defined", rootName),
+		logging.LMKName,
+		rootPos,
+	)
 	return nil, false
 }
