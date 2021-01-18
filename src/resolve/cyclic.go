e87d3eace5f089c34ceb72bb93b6e0aaad2ff84a
ComedicChimera <forlornsisu@gmail.com>
2021-01-18 01:09:35 -0600

    worked more on cyclic resolution algo, worked a bit on opaque types

--- src/resolve/cyclic.go
+++ src/resolve/cyclic.go
@@ -1,11 +1,13 @@
 package resolve
 
+import "github.com/ComedicChimera/whirlwind/src/syntax"
+
 // Cyclic Symbol Resolution Algorithm
 // ----------------------------------
 // 1. Go through all the remaining definitions and collect a table of type
-//    definitions indicating which symbols they depend on (efficiently
-//    selecting the relevant dependency relationships).
-// 2. Iterate through all the type definitions in the table and:
+//    and interface definitions indicating which symbols they depend on
+//    (efficiently selecting the relevant dependency relationships).
+// 2. Iterate through all the definitions in the table and:
 //    a. If the definition depends on a symbol which is not listed in the
 //       table, log it as unresolved and remove it from the table and list
 //       of definitions.
@@ -18,7 +20,7 @@ package resolve
 //    and store the result. Then, check what definitions remain and:
 //    a. If no definitions remain, appropriately log the operand if it
 //       fails to resolve and return the operand resolution result.
-//    b. If only non-type-definitions remain, mark all remaining
+//    b. If only non-type/interf-definitions remain, mark all remaining
 //       definitions as unresolved.  Then, appropriately log the operand
 //       if it fails to resolve and return the operand resolution result
 //    c. If some type definitions remain, proceed to step 5.
@@ -30,6 +32,11 @@ package resolve
 // resolveCyclic provides a mechanism for resolving symbols that are cyclically
 // dependent on each other as described in the algorithm above.
 func (r *Resolver) resolveCyclic() bool {
+	for true {
+		table := r.buildCyclicDefTable()
+		_ = table
+	}
+
 	// Code to Log Errors:
 	// for _, pa := range r.Assemblers {
 	// 	if pa.DefQueue.Len() > 0 {
@@ -38,4 +45,59 @@ func (r *Resolver) resolveCyclic() bool {
 	// }
 
 	return false
+}
+
+// buildCyclicDefTable constructs the table of the type and interface
+// definitions and their dependencies for cyclic resolution (step 1).  This
+// table is organized first by package to prevent name conflicts.
+func (r *Resolver) buildCyclicDefTable() map[uint]map[string]*Definition {
+	table := make(map[uint]map[string]*Definition)
+
+	for _, pa := range r.Assemblers {
+		if pa.DefQueue.Len() > 0 {
+			// to iterate through the queue, we take a starting definition and
+			// rotate through the queue until we hit it a second time
+			start := pa.DefQueue.Peek()
+			var curr *Definition
+
+			for curr != start {
+				curr = pa.DefQueue.Peek()
+				currBranch := curr.Branch
+
+				// annotated definitions can contain type defs and interf defs;
+				// we want to extract and examine the inner definition
+				if currBranch.Name == "annotated_def" {
+					currBranch = curr.Branch.BranchAt(curr.Branch.Len() - 1)
+				}
+
+				if currBranch.Name == "type_def" || currBranch.Name == "interf_def" {
+					// we need to extract the name of the definition before we
+					// can put it in the table.  NOTE: should `Name` by a field
+					// of `Definition`?
+					var name string
+					if currBranch.LeafAt(1).Kind == syntax.TYPE {
+						name = currBranch.LeafAt(2).Value
+					} else {
+						name = currBranch.LeafAt(1).Value
+					}
+
+					// we first need to find the package specific table or
+					// create one if it doesn't already exist and then insert
+					// our definition into that
+					if pkgTable, ok := table[pa.PackageRef.PackageID]; ok {
+						pkgTable[name] = curr
+					} else {
+						pkgTable := make(map[string]*Definition)
+						pkgTable[name] = curr
+						table[pa.PackageRef.PackageID] = pkgTable
+					}
+				}
+
+				pa.DefQueue.Rotate()
+			}
+
+		}
+	}
+
+	return table
 }
--- src/typing/opaque.go
+++ src/typing/opaque.go
@@ -1,7 +1,5 @@
 package typing
 
-// TODO: how do we make this work with generics?
-
 // OpaqueType is used to represent a type that has yet to be defined but is
 // needed to define other types.  It helps to facilitate mutual/cyclic
 // dependency resolution between individual symbols
@@ -25,3 +23,40 @@ type OpaqueType struct {
 // All of OpaqueType's methods treat it as if it is it's evaluated type.  If it
 // has yet to be evaluated, the only method that works is `Equals` since it can
 // simply check if the definitions match exactly
+
+// OpaqueGenericType is a special variant of the standard opaque type that works
+// for generics.  It effectively creates links to all of the generate types so
+// that any errors that occurred when creating the generate that were not
+// detected because the generic did not yet exist can be rectified.  Unlike
+// OpaqueType, this type is not meant to linger in the type system -- its is
+// only used to facilitate opaque behavior and detect when an opaque generate is
+// necessary.
+type OpaqueGenericType struct {
+	// EvalType works the same as it does in `OpaqueType` except it is
+	// explicitly generic
+	EvalType *GenericType
+
+	// These two fields act the exact same as they do in `OpaqueType`
+	DependsOn   map[string]struct{}
+	RequiresRef bool
+
+	// This field stores all of the generate instances that were created based
+	// on this opaque type
+	Generates []*OpaqueGenerateType
+}
+
+// OpaqueGenerateType is used as a temporary placeholder for a generate that is created
+// based off of a generic opaque type.  These types can linger in the type system but
+// are innately memoized to prevent generating unnecessary generics over and over.
+type OpaqueGenerateType struct {
+	// Generic stores a reference to the generic that this generate spawns from.  It
+	// should be the same reference stored in the corresponding `OpaqueGenericType`
+	Generic *GenericType
+
+	// TypeParams is the slice of type parameters passed in to create this generate
+	TypeParams []DataType
+
+	// MemoizedGenerate stores the final/finished generate created after the
+	// opaque generic is evaluated
+	MemoizedGenerate DataType
+}
